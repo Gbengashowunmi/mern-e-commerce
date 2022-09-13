0c895a1aa39428d1e3d013a3ee69fa937897c923
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-09-13 16:55:22 +0100

    my-first-commit

---
+++ e-commerce-folder/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Ma'ruf</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
