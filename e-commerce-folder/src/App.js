ef76aba48f45d64f0f1cb429d34d653d01ca4eca
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-09-14 12:37:13 +0100

    progress

--- e-commerce-folder/src/App.js
+++ e-commerce-folder/src/App.js
@@ -1,12 +1,25 @@
+import data from "./Data";
 
 function App() {
   return (
     <div>
       <header>
-        <a href='/'>Ma'ruf</a>
+        <a href="/">Ma'ruf</a>
       </header>
       <main>
-        List Product
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {data.products.map((product) => (
+            <div className='product' key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div className='product-desc'>
+              <p>{product.name}</p>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ e-commerce-folder/src/Data.js
@@ -0,0 +1,66 @@
+ const data ={
+    products:[
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: 'https://raw.githubusercontent.com/basir/amazona/master/frontend/public/images/p1.jpg',
+            price: 120,
+            countInstock: 10,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality shirt'
+        },
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Pants',
+            image: 'https://github.com/basir/amazona/blob/master/frontend/public/images/p2.jpg?raw=true',
+            price: 250,
+            countInstock: 20,
+            brand:'Nike',
+            rating: 4.0,
+            numReviews: 10,
+            description:'high quality product'
+        },
+        {
+            name: 'Adidas Fit pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: 'https://github.com/basir/amazona/blob/master/frontend/public/images/p3.jpg?raw=true',
+            price: 35,
+            countInstock: 15,
+            brand:'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality shirt'
+        },
+        {
+            name: 'Adidas Fit pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: 'https://github.com/basir/amazona/blob/master/frontend/public/images/p4.jpg?raw=true',
+            price: 35,
+            countInstock: 15,
+            brand:'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality shirt'
+        },
+        {
+            name: 'Adidas Fit pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: 'https://github.com/basir/amazona/blob/master/frontend/public/images/p5.jpg?raw=true',
+            price: 35,
+            countInstock: 15,
+            brand:'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality shirt'
+        },
+    ]
+}
+
+export default data
